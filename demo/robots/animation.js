77f08c1b4d2dfd83c9c5878daf705ba8dd7a13a6
Szymon Witamborski <szywon@szywon.pl>
2013-03-29 23:45:57 +0000

    Added premultiplied translation to the projection matrix to allow objects to be at (0,0,0) spot.

--- demo/robots/animation.js
+++ demo/robots/animation.js
@@ -19,10 +19,9 @@ ns('animation', function () {
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
     gl.clearColor(0, 0, 0, 1);
 
-    var p =  matrices.frustum(5, 5, 3, 500);
-    var mv = matrices.translate(0, 0, 6);
-
-    console.log(p, mv);
+    var p =  matrices.multiply(matrices.frustum(5, 5, 3, 500),
+                               matrices.translate(0, 0, 6));
+    var mv = matrices.identity();
 
     gl.uniform4f(program.color, 1, 1, 1, 1);
     gl.uniformMatrix4fv(program.p,  false, p);
