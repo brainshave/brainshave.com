69795f8f45ecaec83013d237e28a6b5b14967256
Szymon Witamborski <szywon@szywon.pl>
2013-03-31 22:45:18 +0100

    Full bot.

--- demo/robots/bot.js
+++ demo/robots/bot.js
@@ -11,18 +11,23 @@ ns('bot', function () {
 
     var feet_size = matrices.scale(0.3, 0.3, 1);
     var to_feet_joint = matrices.translate(0, 0, 0.2);
-    var ankle_rotate = matrices.rotate_x(Math.PI * 5 / 8);
 
     var to_shin_centre = matrices.translate(0, 0, 1.35);
     var to_leg_part_centre = matrices.translate(0, 0, 1.15);
     var leg_part_size = matrices.scale(0.3, 0.3, 2);
 
-    var knee_rotate = matrices.rotate_x(- Math.PI * 2 / 8);
-
     var to_leg_head_joint = matrices.translate(0, 0, 0.85);
-    var leg_unrotate = matrices.rotate_x(Math.PI / 8);
+
+    var to_head_centre = matrices.translate(1, 0, 0);
     var head_size = matrices.scale(1.5, 1.5, 1.5);
-    var to_head_centre = matrices.translate(0.95, 0, 0);
+
+    var right_ankle_rotate = matrices.rotate_x(Math.PI * 5 / 8);
+    var right_knee_rotate = matrices.rotate_x(- Math.PI * 2 / 8);
+    var right_leg_unrotate = matrices.rotate_x(Math.PI * 5 / 8);
+
+    var left_leg_rotate = matrices.rotate_x(Math.PI * 3 / 8);
+    var left_knee_rotate = matrices.rotate_x(Math.PI * 2 / 8);
+    var left_ankle_rotate = matrices.rotate_x(- Math.PI * 5 / 8);
 
     function set_mv () {
       gl.uniformMatrix4fv(program.mv, false, mv.current());
@@ -41,7 +46,7 @@ ns('bot', function () {
 
       mv.switch(); // undo size
       op(to_feet_joint);
-      op(ankle_rotate);
+      op(right_ankle_rotate);
       op(to_shin_centre);
       op(leg_part_size);
       set_mv();
@@ -49,7 +54,7 @@ ns('bot', function () {
 
       mv.switch();
       op(to_leg_part_centre);
-      op(knee_rotate);
+      op(right_knee_rotate);
       op(to_leg_part_centre);
       op(leg_part_size);
       set_mv();
@@ -57,12 +62,35 @@ ns('bot', function () {
 
       mv.switch();
       op(to_leg_head_joint);
-      op(leg_unrotate);
+      op(right_leg_unrotate);
       op(to_head_centre);
       op(head_size);
       set_mv();
       cube();
 
+      mv.switch();
+      op(to_head_centre);
+      op(left_leg_rotate);
+      op(to_leg_head_joint);
+      op(leg_part_size);
+      set_mv();
+      cube();
+
+      mv.switch();
+      op(to_leg_part_centre);
+      op(left_knee_rotate);
+      op(to_leg_part_centre);
+      op(leg_part_size);
+      set_mv();
+      cube();
+
+      mv.switch();
+      op(to_shin_centre);
+      op(left_ankle_rotate);
+      op(to_feet_joint);
+      op(feet_size);
+      set_mv();
+      cube();
     };
   }
 });
