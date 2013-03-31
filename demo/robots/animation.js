c212bdd3af0ec57f98d83abd18ab38c4d1e86e52
Szymon Witamborski <szywon@szywon.pl>
2013-03-31 15:59:58 +0100

    Calculating the view size for correct visibility and proportions.

--- demo/robots/animation.js
+++ demo/robots/animation.js
@@ -10,18 +10,23 @@ ns('animation', function () {
 
     var mv = matrices.switcher();
 
-    var angle = matrices.rotate_x(Math.PI/100);
-
+    var angle = matrices.multiply(matrices.rotate_x(Math.PI/800),
+                                  matrices.rotate_y(Math.PI/200),
+                                  matrices.rotate_z(Math.PI/100));
     step();
 
     function step () {
       matrices.multiply(mv.current(), angle, mv.switch());
       gl.uniformMatrix4fv(program.mv, false, mv.current());
 
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      cube.draw(gl.LINES);
+      draw();
 
       requestAnimationFrame(step);
+    }
+
+    function draw () {
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      cube.draw(gl.LINES);
     }
   }
 
@@ -32,10 +37,31 @@ ns('animation', function () {
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
     gl.clearColor(0, 0, 0, 1);
 
-    var p =  matrices.multiply(matrices.frustum(5, 5, 3, 500),
-                               matrices.translate(0, 0, 6));
+    var size = near_plane_size(16, 9, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    var p = matrices.multiply(matrices.frustum(size.w, size.h, 3, 500),
+                              matrices.translate(0, 0, 6));
 
     gl.uniform4f(program.color, 1, 1, 1, 1);
     gl.uniformMatrix4fv(program.p,  false, p);
+  }
+
+  /**
+     Takes the desired view size (in OpenGL world units) and viewport
+     size. Returns size of the view (again in the OpenGL units)
+     outgrown in one direction so whole view is visible and original
+     proportions are kept. (useful for frustum function)
+  */
+  function near_plane_size (w, h, W, H) {
+    var r = w / h;
+    var R = W / H;
+
+    return r > R ? {
+      w: h,
+      h: h / R
+    } : {
+      w: w * R,
+      h: w
+    };
   }
 });
