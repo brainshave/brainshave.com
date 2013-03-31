acf7d3bcd0617fdb2c3e98203dbfdf2738c7c897
Szymon Witamborski <szywon@szywon.pl>
2013-03-31 22:58:16 +0100

    Function gathering series of matrix multiplication + centering bot preview.

--- demo/robots/bot.js
+++ demo/robots/bot.js
@@ -7,6 +7,8 @@ ns('bot', function () {
     var mv = matrices.switcher();
     var cube = elements.cube(gl, program);
 
+    var invert_x = matrices.scale(-1, 1, 1);
+
     var cube_size_forward = matrices.translate(0, 0, 2);
 
     var feet_size = matrices.scale(0.3, 0.3, 1);
@@ -37,60 +39,62 @@ ns('bot', function () {
       matrices.multiply(mv.current(), other, mv.switch());
     }
 
+    function series () {
+      mv.switch();
+      for (var i = 0; i < arguments.length; ++i) {
+        op(arguments[i]);
+      }
+      set_mv();
+      cube();
+    }
+
     return function draw (start_mv) {
       mv.current().set(start_mv);
 
+      // centering preview.
+      op(invert_x);
+      op(to_head_centre);
+      op(invert_x);
+
       op(feet_size);
       set_mv();
       cube();
 
-      mv.switch(); // undo size
-      op(to_feet_joint);
-      op(right_ankle_rotate);
-      op(to_shin_centre);
-      op(leg_part_size);
-      set_mv();
-      cube();
+      series(
+        to_feet_joint,
+        right_ankle_rotate,
+        to_shin_centre,
+        leg_part_size);
 
-      mv.switch();
-      op(to_leg_part_centre);
-      op(right_knee_rotate);
-      op(to_leg_part_centre);
-      op(leg_part_size);
-      set_mv();
-      cube();
+      series(
+        to_leg_part_centre,
+        right_knee_rotate,
+        to_leg_part_centre,
+        leg_part_size);
 
-      mv.switch();
-      op(to_leg_head_joint);
-      op(right_leg_unrotate);
-      op(to_head_centre);
-      op(head_size);
-      set_mv();
-      cube();
+      series(
+        to_leg_head_joint,
+        right_leg_unrotate,
+        to_head_centre,
+        head_size);
 
-      mv.switch();
-      op(to_head_centre);
-      op(left_leg_rotate);
-      op(to_leg_head_joint);
-      op(leg_part_size);
-      set_mv();
-      cube();
+      series(
+        to_head_centre,
+        left_leg_rotate,
+        to_leg_head_joint,
+        leg_part_size);
 
-      mv.switch();
-      op(to_leg_part_centre);
-      op(left_knee_rotate);
-      op(to_leg_part_centre);
-      op(leg_part_size);
-      set_mv();
-      cube();
+      series(
+        to_leg_part_centre,
+        left_knee_rotate,
+        to_leg_part_centre,
+        leg_part_size);
 
-      mv.switch();
-      op(to_shin_centre);
-      op(left_ankle_rotate);
-      op(to_feet_joint);
-      op(feet_size);
-      set_mv();
-      cube();
+      series(
+        to_shin_centre,
+        left_ankle_rotate,
+        to_feet_joint,
+        feet_size);
     };
   }
 });
