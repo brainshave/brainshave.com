0aa66f72b79093aca72510742cd1536e2eeb1990
Szymon Witamborski <szywon@szywon.pl>
2013-11-25 22:59:17 +0000

    Update to icons: chopped upper 1px margin.

--- lib/gen_icons_css.sh
+++ lib/gen_icons_css.sh
@@ -19,7 +19,7 @@ function svg {
 <?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
   "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg width="16" height="16"
+<svg width="16" height="15"
      xmlns="http://www.w3.org/2000/svg"
      version="1.1">
 EOF
