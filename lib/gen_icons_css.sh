0421f99cea354868cde399e806b0ac9f92c1ec84
Szymon Witamborski <szywon@szywon.pl>
2014-02-17 22:51:03 +0000

    Using text encoding for svgs gzips better.

--- lib/gen_icons_css.sh
+++ lib/gen_icons_css.sh
@@ -16,33 +16,31 @@ function svg {
   name=$1
 
   convert icons/$name.gif /tmp/icon_$name.svg
-  cat <<EOF
+  echo $(cat <<EOF
 <?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
-  "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg width="16" height="15"
-     xmlns="http://www.w3.org/2000/svg"
-     version="1.1">
+"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+<svg width="16" height="15" xmlns="http://www.w3.org/2000/svg" version="1.1">
 EOF
-  cat /tmp/icon_$name.svg | \
+    cat /tmp/icon_$name.svg | \
     sed 's/circle/rect/g' | \
-    sed 's/cy/y/g' | \
-    sed 's/cx/x/g' | \
-    sed 's/r="1"/width="1" height="1"/g' | \
-    grep black
-  echo "</svg>"
+      sed 's/cy/y/g' | \
+      sed 's/cx/x/g' | \
+      sed 's/r="1"/width="1" height="1"/g' | \
+      grep black
+echo "</svg>") | \
+      sed 's/> </></g'
 }
 
 function css {
   name=$1
 
   data_gif=$(base64 < icons/$name.gif)
-  data_svg=$(base64 < icons/$name.svg)
+  data_svg="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$(cat icons/$name.svg)")"
 
   echo ".icon.${name} {"
   echo "  background-image: url(/icons/$name.gif);"
-  echo "  background-image: url(data:image/gif;base64,$data_gif);"
-  echo "  background-image: url(data:image/svg+xml;base64,$data_svg);"
+  echo "  background-image: url(data:image/svg+xml;charset=UTF-8,$data_svg);"
   echo "}"
   echo
 }
