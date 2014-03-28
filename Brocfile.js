d524178c38d4fcc4cd921c910b47ea92a7a94978
Szymon Witamborski <szywon@szywon.pl>
2014-03-28 21:50:00 +0000

    Generate headers for md files.

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,16 @@
 "use strict";
 
+var mds_w_headers = require("./lib/mds_w_headers");
+
 var stencil = require("broccoli-stencil");
 var less = require("broccoli-less");
 var concat = require("broccoli-concat");
 
 module.exports = function (broccoli) {
+  var pages_w_mds_w_headers = mds_w_headers(broccoli.makeTree("pages"));
+
   var pages = stencil({
-    pages:     broccoli.makeTree("pages"),
+    pages:     pages_w_mds_w_headers,
     templates: broccoli.makeTree("templates"),
     partials:  broccoli.makeTree("partials")
   });
