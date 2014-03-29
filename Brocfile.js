5e09fce02f63925d94fcb31c3819a5830f4a8ce8
Szymon Witamborski <szywon@szywon.pl>
2014-03-29 01:49:04 +0000

    Create indexes for directories.

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var mds_w_headers = require("./lib/mds_w_headers");
+var indexes = require("./lib/indexes");
 
 var stencil = require("broccoli-stencil");
 var less = require("broccoli-less");
@@ -8,9 +9,12 @@ var concat = require("broccoli-concat");
 
 module.exports = function (broccoli) {
   var pages_w_mds_w_headers = mds_w_headers(broccoli.makeTree("pages"));
+  var indexed = indexes(pages_w_mds_w_headers, { template: "list" });
 
+  var input_pages = new broccoli.MergedTree([pages_w_mds_w_headers,
+                                             indexed]);
   var pages = stencil({
-    pages:     pages_w_mds_w_headers,
+    pages:     input_pages,
     templates: broccoli.makeTree("templates"),
     partials:  broccoli.makeTree("partials")
   });
