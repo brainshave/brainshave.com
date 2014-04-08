6269a1966cc956a7b5bcbb926a020fd9c4fe3a27
Szymon Witamborski <szywon@szywon.pl>
2014-04-08 17:37:16 +0100

    Adapt Brocfile to new version of broccoli.

--- Brocfile.js
+++ Brocfile.js
@@ -7,41 +7,42 @@ var beauty = require("./lib/beauty");
 var stencil = require("broccoli-stencil");
 var less = require("broccoli-less");
 var concat = require("broccoli-concat");
+var merge = require("broccoli-merge-trees");
 
 module.exports = function (broccoli) {
   // Add headers to md files
-  var input_pages_w_headers = mds_w_headers(broccoli.makeTree("pages"));
+  var input_pages_w_headers = mds_w_headers("pages");
 
-  var input_pages = new broccoli.MergedTree([
+  var input_pages = merge([
     input_pages_w_headers,
     // Create indexes for folders (like "blog" and "talks")
     indexes(input_pages_w_headers, { template: "list" })
   ]);
 
   // Allow to use pages as partials
-  var partials = new broccoli.MergedTree([
-    broccoli.makeTree("partials"),
+  var partials = merge([
+    "partials",
     input_pages
   ]);
 
   var pages = beauty(stencil({
     pages:     input_pages,
     partials:  partials,
-    templates: broccoli.makeTree("templates")
+    templates: "templates"
   }));
 
-  var css = concat(less(broccoli.makeTree("styles"), {
+  var css = concat(less("styles", {
     paths: "./styles"
   }), {
     inputFiles: ["*.css"],
     outputFile: "/all.css"
   });
 
-  var js = concat(broccoli.makeTree("app"), {
+  var js = concat("app", {
     // Make sure ns.js is first
     inputFiles: ["ns.js", "*.js"],
     outputFile: "/all.js"
   });
 
-  return [css, js, pages];
+  return merge([css, js, pages]);
 };
