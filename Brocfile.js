fecff4f4ac2cde8e8dbfa0e284627611e5bbe1de
Szymon Witamborski <szywon@szywon.pl>
2014-07-09 16:40:23 +0100

    Adapt bunch of stuff to broccoli-dep-filter.

--- Brocfile.js
+++ Brocfile.js
@@ -4,18 +4,24 @@ var mds_w_headers = require("./lib/mds_w_headers");
 var indexes = require("./lib/indexes");
 var beauty = require("./lib/beauty");
 var atom = require("./lib/atom");
+var merge = require("./lib/copy");
 
 var stencil = require("broccoli-stencil");
 var less = require("broccoli-more");
 var concat = require("broccoli-concat");
-var merge = require("broccoli-merge-trees");
 var copy = require("broccoli-static-compiler");
 
 var input_pages_w_headers = mds_w_headers("pages");
 var indexes = indexes(input_pages_w_headers, { template: "list" });
 
-var input_pages = merge([indexes, input_pages_w_headers]);
+var root_pages = merge(["pages"], {
+  filter: function (path) {
+    return /^[^\/]+$/.test(path);
+  }
+});
+var input_pages = merge([root_pages, indexes, input_pages_w_headers]);
 
+module.exports = input_pages;
 // Allow to use pages as partials
 var partials = merge([
   "partials",
@@ -30,19 +36,21 @@ var pages = beauty(stencil({
 
 var atoms = atom({ indexes: indexes, pages: pages });
 
-var images = copy("pages", {
-  srcDir: "/", destDir: "/",
-  files: ["**/*.png", "**/*.jpg", "**/*.svg"]
+var images = merge(["pages"], {
+  extensions: ["png", "jpg", "svg"]
 });
 
-var icons = copy("icons", {
-  srcDir: "/", destDir: "/icons",
-  files: ["**/*.gif", "**/*.ico"]
+var icons = merge(["icons"], {
+  dest_dir: "icons",
+  extensions: ["gif", "ico"]
 });
 
-var cname = copy("resources", {
-  srcDir: "/", destDir: "/",
-  files: ["CNAME"]
+var demos = merge(["demo"], {
+  dest_dir: "demo"
+});
+
+var cname = merge(["resources"], {
+  filter: /^CNAME$/
 });
 
 var css = concat(less("styles", {
@@ -52,15 +60,11 @@ var css = concat(less("styles", {
   outputFile: "/all.css"
 });
 
-var demos = copy("demo", {
-  srcDir: "/", destDir: "/demo",
-  files: ["**/*.*"]
-});
-
 var js = concat("app", {
   // Make sure ns.js is first
   inputFiles: ["ns.js", "*.js"],
   outputFile: "/all.js"
 });
 
-module.exports = merge([css, js, pages, atoms, images, icons, cname, demos]);
+module.exports = merge([css, js, pages, atoms,
+                        images, icons, cname, demos]);
